45aeda96a02d2fb4d62082464077e1930e2d21fd
Jens <jens.jens@live.dk>
2024-12-06 06:38:23 +0100

    chore(flake): smart flake with flake-utils

--- flake.nix
+++ flake.nix
@@ -1,35 +1,22 @@
-# {
-#   description = "gleam";
-
-#   inputs = {
-#     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-#     flake-utils.url = "github:numtide/flake-utils";
-#   };
-
-#   outputs = { self, nixpkgs, flake-utils, ... }@inputs:
-#     flake-utils.lib.eachDefaultSystem (system:
-#       let
-#         pkgs = import inputs.nixpkgs { inherit system; };
-#         dev-deps = with pkgs; [ gleam erlang ];
-#       in with pkgs; {
-#         formatter.${system} = pkgs.alejandra;
-#         devShells.default = pkgs.mkShell rec {
-#           nativeBuildInputs = [ pkgs.pkg-config ];
-#           buildInputs = dev-deps;
-#           LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
-#         };
-#       });
-# }
 {
-  inputs.gleam-nix.url = "github:vic/gleam-nix";
+  description = "gleam";
 
-  outputs = { gleam-nix, nixpkgs, ... }:
-    let
-      system = "aarch64-darwin"; # or anything you use.
-      pkgs = import nixpkgs {
-        inherit system;
+  inputs = {
+    gleam-nix.url = "github:vic/gleam-nix";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, gleam-nix, flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
         overlays = [ gleam-nix.overlays.default ];
-      };
-    in { packages.${system}.gleam = pkgs.gleam; };
+        pkgs = import nixpkgs { inherit system overlays; };
+      in with pkgs; {
+        devShells.default = pkgs.mkShell rec {
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          buildInputs = [ pkgs.gleam ];
+          LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+        };
+      });
 }
 
